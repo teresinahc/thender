c71af1cacb11c285c603e19350d82e5d6bb07a77
Renato Alencar <renatoalencar.73@gmail.com>
2016-03-05 18:22:32 -0300

    Fix serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,8 @@
 from .models import Candidato
 from rest_framework import serializers
 
-class CandidatoSerializer(serializers.HyperlinkedModelSerializer):
+class CandidatoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidato
         fields = ('nome', 'foto', 'legenda', 'nascimento', 'formacao', 'renda_declarada', 'posicionamento')
-
+        depth = 1
