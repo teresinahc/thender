d30640637363247bb0e87f7ec2dd0abb84b47e66
Renato Alencar <renatoalencar.73@gmail.com>
2016-03-05 18:20:40 -0300

    Adiciona outros models ao admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Candidato
+from .models import Candidato, Legenda, Posicionamento
 
 # Register your models here.
 admin.site.register(Candidato)
+admin.site.register(Legenda)
+admin.site.register(Posicionamento)
