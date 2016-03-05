fbc9fda8486d8843f9d0f3834baa314a75f15a41
Renato Alencar <renatoalencar.73@gmail.com>
2016-03-05 15:46:43 -0300

    Ajeita estrutura dos models
    * Muda legenda para model
    * Cria index no campo nome
    * Muda ordering de Candidato

--- core/models.py
+++ core/models.py
@@ -7,15 +7,25 @@ class Posicionamento(models.Model):
     def __str__(self):
         return self.tag
 
+class Legenda(models.Model):
+    nome = models.CharField(max_length=100, blank=True)
+    sigla = models.CharField(max_length=8, blank=True)
+
+    def __str__(self):
+        return '%s-%s' % (self.nome, self.sigla)
+
 class Candidato(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, db_index=True)
     foto = models.ImageField(upload_to='uploads/candidatos/',
                              blank=True, null=True)
-    legenda = models.CharField(max_length=100, blank=True)
+    legenda = models.ForeignKey(Legenda)
     nascimento = models.DateField()
     formacao = models.CharField(max_length=100, blank=True)
     renda_declarada = models.FloatField()
     posicionamento = models.ManyToManyField(Posicionamento, blank=True)
+
+    class Meta:
+        ordering = ('nome', 'legenda')
 
     def __str__(self):
         return self.nome
